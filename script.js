97448ebe4cdfeb78e13c2075c97d854d378d6477
nephtalir6155 <73478976+nephtalir6155@users.noreply.github.com>
2020-10-26 09:33:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Nephtali R"
+let userName = "nephtalir6155";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
